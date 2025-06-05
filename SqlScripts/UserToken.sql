22d4c3b50d2427b76a8658189639d06a0ca9da9a
Vandana Chowdary <168184331+vandanachowdary16@users.noreply.github.com>
2025-06-05 05:34:20 +0530

    Added entities using reverse engineering

---
+++ SqlScripts/UserToken.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE [dbo].[UserTokens](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NULL,
+	[AccessToken] [nvarchar](2000) NULL,
+	[RefreshToken] [nvarchar](1000) NULL,
+	[RefreshTokenExpiryTime] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
